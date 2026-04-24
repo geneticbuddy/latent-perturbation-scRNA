5d047de0d46119474579049fab897f771b7bc68c
hernandezavery <64673650+geneticbuddy@users.noreply.github.com>
2026-04-24 08:44:22 -0400

    Create figure2.py

---
+++ figure2.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+expected = 0.0016
+observed = 0.0015
+
+plt.bar(["Expected", "Observed"], [expected, observed])
+
+plt.ylabel("Shift Magnitude")
+plt.title("Figure 2")
+
+plt.savefig("Figure2.png", dpi=300)
+plt.show()
